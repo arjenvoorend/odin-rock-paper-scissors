38788bbbb665a62c78b55df28eaf101642e254a9
Arjen Voorend <a.o.voorend@gmail.com>
2022-06-08 15:31:33 +0200

    Add JS playRound function

--- app.js
+++ app.js
@@ -20,3 +20,26 @@ function computerPlay() {
   // return the chosen value
   return computerPick;
 }
+
+// Write a function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // Compare player and computer picks and determine the winner
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both chose ${playerSelection}.`
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You lose! Paper beats Rock."
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You win! Rock beats Scissors."
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You win! Paper beats Rock."
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You lose! Scissors beats Paper."
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You lose! Rock beats Scissors"
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You win! Scissors beats Paper."
+  }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
