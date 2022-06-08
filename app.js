c2ccc950a606f3ef7e6913a414d487aa7f7b3fb6
Arjen Voorend <a.o.voorend@gmail.com>
2022-06-08 16:03:04 +0200

    Add JS game function

--- app.js
+++ app.js
@@ -23,23 +23,57 @@ function computerPlay() {
 
 // Write a function that plays a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
+  // Score counter
+
   // Compare player and computer picks and determine the winner
   if (playerSelection === computerSelection) {
-    return `It's a tie! You both chose ${playerSelection}.`
+    console.log(`It's a tie! You both chose ${playerSelection}.`)
   } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    return "You lose! Paper beats Rock."
+    computerScore++;
+    console.log("You lose! Paper beats Rock.")
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    return "You win! Rock beats Scissors."
+    playerScore++;
+    console.log("You win! Rock beats Scissors.")
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return "You win! Paper beats Rock."
+    playerScore++;
+    console.log("You win! Paper beats Rock.")
   } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    return "You lose! Scissors beats Paper."
+    computerScore++;
+    console.log("You lose! Scissors beats Paper.")
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You lose! Rock beats Scissors"
+    computerScore++;
+    console.log("You lose! Rock beats Scissors")
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You win! Scissors beats Paper."
+    playerScore++;
+    console.log("You win! Scissors beats Paper.")
+  }
+  console.log(`player score: ${playerScore}`)
+  console.log(`computer score: ${computerScore}`)
+}
+
+
+
+
+// Write a function that plays 5 rounds of Rock Paper Scissors and reports a winner and loser at the end
+function game() {
+  // Play 5 rounds of Rock Paper Scissors
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = window.prompt("Rock, Paper or Scissors?");
+
+    playRound(playerSelection, computerPlay())
+  }
+
+  // Announce the winner
+  if (playerScore > computerScore) {
+    console.log("Game over, you win!")
+  } else if (playerScore < computerScore) {
+    console.log("Game over, you lose!")
+  } else {
+    console.log("Game over, it's a tie!")
   }
 }
 
-const playerSelection = "Rock";
-const computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+
+game();
